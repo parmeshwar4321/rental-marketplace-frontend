f33e64387093fd51683106f79e6689264c75cd90
Hahz <hahz5d@pm.me>
2022-04-06 15:32:08 -0400

    create

--- pages/create-item.js
+++ pages/create-item.js
@@ -106,7 +106,7 @@ export default function CreateItem() {
 
         <label htmlFor="AssetDescription" className="my-6 block mb-2 text-lg font-medium text-white ">🤍NFT RENTAL DEETS</label>
         <textarea
-          placeholder="Write a short description of your 🤍NFT rental"
+          placeholder="A short description of your 🤍NFT rental."
           className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
         />
